5f34ba441ebd03b464c5c68ebb3e7cad99a958a3
Ian Whitmore <ianwhitmor@gmail.com>
2021-08-09 23:21:16 -0500

    add js total clicks

--- js/app.js
+++ js/app.js
@@ -49,3 +49,45 @@ rightProductOnPage = ProductPicture.allImages[rightIndex];
 renderNewProducts(leftIndex,rightIndex);
 };
 
+const handleClickonProduct = function(event){
+  console.log('Lets handle the click now');
+  console.log('left product on the page. ', leftProductOnPage);
+
+  if (totalClicks < 5){
+
+    const thingWeClickOn = event.target;
+    console.log('event target', event.target);
+    const id = thingWeClickOn.id;
+    console.log('thingWeClickOn', thingWeClickOn);
+    console.log('this is the id', id);
+
+    if(id === 'left_product_img'){
+      console.log('left product on the page.', leftProductOnPage);
+      leftProductOnPage.clicks++;
+    }
+    if(id === 'middle_product_img'){
+      console.log('middle product on the page.', middleProductOnPage);
+      middleProductOnPage.clicks++;
+    }
+    if(id === 'right_product_img'){
+      console.log('right prodcut on the page.', rightProductOnPage);
+      rightProductOnPage.clicks++;
+    }
+    console.log('left product on the page. ', leftProductOnPage);
+    leftProductOnPage.timesShown++;
+    middleProductOnPage.timesShown++;
+    rightProductOnPage.timesShown++;
+    pickNewProducts();
+  }
+  console.log('is this running ',event.target.id);
+}
+
+totalClicks++;
+if(totalClicks === 5){
+  productImageSectionTag.removeEventListener('click', handleClickonProduct);
+  console.log('the vote has ended. and remove listener works. ');
+}
+
+productImageSectionTag.addEventListener('click', handleClickonProduct);
+
+
